70629e7ce2c0e09c8fc56c1e2406a5f88cf8f4a1
XiaoZe980 <2580792919@qq.com>
2026-04-22 00:16:37 +0800

    1、增加了鼠标放置在敌人身上高亮功能 2、GAS系统，创建了PlayerState

---
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -0,0 +1,116 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/AuraPlayerController.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "Interaction/EnemyInterface.h"
+
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates = true;
+}
+
+void AAuraPlayerController::PlayerTick(float DeltaTime)
+{
+	Super::PlayerTick(DeltaTime);
+	
+	CursorTrace();
+}
+
+void AAuraPlayerController::CursorTrace()
+{
+	FHitResult CursorHit;
+	GetHitResultUnderCursor(ECC_Visibility, false, CursorHit);
+	if (!CursorHit.bBlockingHit)return;
+	
+	LastActor = ThisActor;
+	ThisActor = Cast<IEnemyInterface>(CursorHit.GetActor());
+	
+	/**
+	 * 从光标处绘制线条，有几种情况
+	 * A. 如果“LastActor”为空且“CurrActor”也为空，则不执行任何操作
+	 * B. 如果“LastActor”为空且“CurrActor”不为空，则高亮显示“CurrActor”
+	 * C. 如果“LastActor”不为空且“CurrActor”为空，则取消高亮显示“LastActor”
+	 * D. 如果“LastActor”和“CurrActor”都不为空，但“LastActor”和“CurrActor”不相同，则取消高亮显示“LastActor”，高亮显示“CurrActor”
+	 * E. 如果“LastActor”和“CurrActor”都不为空，且“LastActor”和“CurrActor”相同，则不执行任何操作
+	 */
+
+	if (LastActor == nullptr)
+	{
+		if (ThisActor != nullptr)
+		{
+			// Case B
+			ThisActor->HighLightActor();
+		}
+		else
+		{
+			// Case A
+		}
+	}
+	else // LastActor 有效
+	{
+		if (ThisActor == nullptr)
+		{
+			// Case C
+			LastActor->UnHighLightActor();
+		}
+		else // LastActor 和 ThisActor 都有效
+		{
+			if (LastActor != ThisActor)
+			{
+				// Case D
+				LastActor->UnHighLightActor();
+				ThisActor->HighLightActor();
+			}
+			else
+			{
+				// Case E
+			}
+		}
+	}
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	check(AuraContext);
+	
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem);
+	Subsystem->AddMappingContext(AuraContext, 0);
+	
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+	
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
+}
+
+void AAuraPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AAuraPlayerController::Move);
+}
+
+void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
+{
+	const FVector2D InputAxisVector = InputActionValue.Get<FVector2D>();
+	const FRotator Rotation = GetControlRotation();
+	const FRotator YawRotation(0.f, Rotation.Yaw, 0.f);
+	
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+	if (APawn* ControlledPawn = GetPawn<APawn>())
+	{
+		ControlledPawn->AddMovementInput(ForwardDirection, InputAxisVector.Y);
+		ControlledPawn->AddMovementInput(RightDirection, InputAxisVector.X);
+	}
+}
+
+
